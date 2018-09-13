62ec27a39674cf9e84a3408de4c3a5d7f1421af8
vinayvanam111 <vinayvanam111@gmail.com>
2018-09-13 19:35:44 +0530

    del

--- RG Kedia College/bootstrap/js/main.js
+++
@@ -1,8 +0,0 @@
-$('.port-item').click(function () {
-    $('.collapse').collapse('hide');
-});
-
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-});
